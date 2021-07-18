a6e036355271172488434394817f1da72b1274f6
samdaviesudina <sam.d@brainlabsdigital.com>
2021-07-18 22:50:10 +0100

    Allow for deletion of multiple flashcards and refactor a bit

--- app/editing_session.py
+++ app/editing_session.py
@@ -35,24 +35,49 @@ class EditingSession:
         if action == "edit":
             raise NotImplementedError
         if action == "add":
-            while True:
-                self.add_flashcard()
+            self._do_adding()
+        if action == "delete":
+            self._do_deleting()
+
+    def _display_all_flashcards_in_collection(self) -> None:
+        for flashcard in self.collection:
+            self.cli.print(f"  {flashcard}")
+
+    def _do_adding(self) -> None:
+        while True:
+            self._add_flashcard()
+            try:
                 wants_to_add_another_one = self.cli.prompt_with_yes_no_question(
                     "Added a new flashcard. Want to add another one?"
                 )
                 if not wants_to_add_another_one:
                     break
-        if action == "delete":
-            self.cli.print("These are the flashcards in your collection:")
-            for flashcard in self.collection:
-                self.cli.print(f"  {flashcard}")
+            except self.cli.NoAnswerProvidedError:
+                break
+
+    def _do_deleting(self) -> None:
+        self.cli.print("These are the flashcards in your collection:")
+        self._display_all_flashcards_in_collection()
+        while True:
             flashcard_id = int(
                 self.cli.prompt("Type in the ID of the flashcard you'd like to delete:")
             )
+            if flashcard_id not in [flashcard.id for flashcard in self.collection]:
+                self.cli.print(
+                    f"The ID '{flashcard_id}' does not match any existing flashcards."
+                )
             self.collection.delete_flashcard(flashcard_id)
-            self.cli.print(f"Flashcard with ID '{flashcard_id}' successfully deleted.")
+            try:
+                wants_to_delete_another_one = self.cli.prompt_with_yes_no_question(
+                    f"Flashcard with ID '{flashcard_id}' successfully deleted."
+                    " Want to delete another one?"
+                )
+                if not wants_to_delete_another_one:
+                    break
+            except self.cli.NoAnswerProvidedError:
+                break
 
-    def add_flashcard(self) -> None:
+    def _add_flashcard(self) -> None:
         question = self.cli.prompt("What's the question?")
         answer = self.cli.prompt("And what's the answer?")
         self.collection.add_flashcard(Question(question), Answer(answer))
